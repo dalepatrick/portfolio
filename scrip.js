dfccdac930029bf087743616ad517a4bcd89cb05
dalepatrick <dalepatrick005@gmail.com>
2023-07-21 00:27:55 +0800

    add js and edit a contact form

--- scrip.js
+++ scrip.js
@@ -27,3 +27,37 @@ document.addEventListener("DOMContentLoaded", function() {
       servicesSection.scrollIntoView({ behavior: "smooth" });
     });
   });
+
+  // contact
+  document.addEventListener("DOMContentLoaded", function() {
+    const servicesLink = document.querySelector('a[href="#contact"]');
+    servicesLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      const servicesSection = document.getElementById("contact");
+      servicesSection.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  // contact
+  function submitForm() {
+    let name = document.getElementById('name').value;
+    let lastName = document.getElementById('lastName').value;
+    let message = document.getElementById('message').value;
+
+    if (name.trim() === '' || lastName.trim() === '' || message.trim() === '') {
+      alert('Please fill in all fields before submitting the form.');
+      return;
+    }
+    
+    document.getElementById('name').value = '';
+    document.getElementById('lastName').value = '';
+    document.getElementById('message').value = '';
+
+    alert('Form submitted successfully!');
+  }
+
+  // Attach an event listener to the Submit button to trigger the form submission function
+  document.getElementById('submitButton').addEventListener('click', function (event) {
+    event.preventDefault(); 
+    submitForm(); 
+  });
